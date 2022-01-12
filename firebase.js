a5bac8f6194d7e341c5ea71fd60fef35e0ee9126
Hasin Mahtub Khan <77059980+Rumon360@users.noreply.github.com>
2022-01-12 19:22:05 +0600

    Deleted Firebase Config info

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD_9J_iGyYbqVoC5gfC62aETuWwuw1UbMI',
-  authDomain: 'chadsonly-88ec6.firebaseapp.com',
-  projectId: 'chadsonly-88ec6',
-  storageBucket: 'chadsonly-88ec6.appspot.com',
-  messagingSenderId: '452268657126',
-  appId: '1:452268657126:web:23a7919208e39d16316e27',
+
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
